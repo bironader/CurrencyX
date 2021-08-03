a332e215153e8d9effef3575f363a9f97a4faab6
biro <bironader123@gmail.com>
2021-08-04 00:17:55 +0200

    room dependencies

---
+++ buildSrc/src/main/java/Room.kt
@@ -0,0 +1,8 @@
+object Room {
+
+    const val ANDROID = "androidx.room:room-runtime:${Versions.ROOM}"
+
+    const val COROUTINES = "androidx.room:room-ktx:${Versions.ROOM}"
+
+    const val COMPILER = "androidx.room:room-compiler:${Versions.ROOM}"
+}
